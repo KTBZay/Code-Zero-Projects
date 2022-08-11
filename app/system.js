af7b002f57ab78a4871b881fe5b511a9bdf06fd3
KTBZay <97396492+KTBZay@users.noreply.github.com>
2022-08-11 16:32:07 -0400

    V1.0.2 OFFICAL
    Added a runtime
     - Added 2 commands
       1. Ping
       2. cVersion
       3. Discordling
    
    All notes updated ay 4:31 8/11/22

--- app/system.js
+++ app/system.js
@@ -24,9 +24,9 @@ setTimeout(() => {
              const config = { token: '${configuration.Discord_Bot_Token}'}\n 
              const client = new Client({ Intents: ["GUILD_MESSAGING", "GUILDS"] }) \n
              client.on('ready', () => {console.log('Logged in')});\n
-             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Token}ping"){msg.reply('Pong')}});\n
-             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Token}cVersion"){msg.reply('1.0.3')}});\n
-             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Token}Support"){msg.reply('Get your questions answered ${UserData.Discord_Support_Link} or Email us at ${UserData.Public_Contact_Email}')}});\n
+             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Prefix}ping"){msg.reply('Pong')}});\n
+             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Prefix}cVersion"){msg.reply('1.0.3')}});\n
+             client.on('message', (msg) => { if(msg.content === "${configuration.Discord_Bot_Prefix}Support"){msg.reply('Get your questions answered ${UserData.Discord_Support_Link} or Email us at ${UserData.Public_Contact_Email}')}});\n
              client.login(config.token)`);
     FileCreator(`${UserData.User_Project_Data.path}/discord_bot.config.js`,
         `const czRunTimeService = require('../../lib/system/Runtime')\n
